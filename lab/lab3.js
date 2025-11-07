8c0f770a119d7273b6f6b735734a1c09931074b0
HoangXuanTruong <haongtruong4567@gmail.com>
2025-11-07 16:08:31 +0700

    up

--- lab/lab3.js
+++ lab/lab3.js
@@ -17,12 +17,44 @@ document.addEventListener("click", () => {
 });
 
 // bài 2
-function createUser(name = "Anonymous", age="18", isAdmin = false) {
-  return {
-   name,
-    age,
-    isAdmin,
-  };
-}
+const createUser = (name = "Anonymous", age = 18, isAdmin = false) => ({
+  name,
+  age,
+  isAdmin,
+});
+
 
 // bài 3
+// 1.Hàm mergeArrays nhận nhiều mảng và trả về mảng hợp nhất
+function mergeArrays(...arrays){
+    return [].concat(...arrays);
+}
+//2.Hàm sumAll tính tổng tất cả tham số truyền vào
+function sumAll(...numbers){
+    return numbers.reduce((total, num) => total + num, 0);
+}
+// 3.Hàm createProduct nhận thông tin sản phẩm và trả về object sản phẩm với giá trị mặc định 
+function createProduct(name= Mao,price = 100,category="General"){
+    return {
+        name,
+        price,
+        category,
+    };
+}
+//bài 4
+//Tạo hàm shoppingCart với:
+// Tham số đầu tiên là tên khách hàng
+// Các tham số tiếp theo là các sản phẩm
+// Sử dụng rest parameters để thu thập sản phẩm
+// Trả về object chứa thông tin đơn hàng
+
+function shoppingCart(customerName, ...products){
+    return {
+        customerName,
+        products,
+    };
+}
+// Ví dụ sử dụng
+const order = shoppingCart("Nguyen Van A", "Laptop", "Mouse", "Keyboard");
+console.log(order);
+// Kết quả:
