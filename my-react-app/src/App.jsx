cd6d6789f65ebf881d76632ff0b320305e02b929
HoangXuanTruong <haongtruong4567@gmail.com>
2025-11-14 15:19:09 +0700

    ok

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  // mock data
+  const tours = [
+    {
+      id: 1,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+    {
+      id: 2,
+      title: 'Tour Du Lịch Hoa Ky',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+    {
+      id: 3,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
+    {
+      id: 4,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
+        Tour Nội Địa
+      </h1>
+      <p className="text-xl font-medium my-2 px-2">
+        Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn sẵn
+        sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách, giúp
+        mang đến những chuyến hành trình tuyệt vời.
       </p>
-    </>
+      <div className="flex gap-2">
+        {tours.map(tour => (
+          <div>
+            <img src={tour.image} width={500} alt="" height={300} />
+            <h2 className="text-3xl font-bold text-orange-300">{tour.title}</h2>
+          </div>
+        ))}
+
+        {/* <div>
+          <img src={tours[0].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[0].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[1].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[1].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[2].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[2].title}
+          </h2>
+        </div> */}
+      </div>
+    </div>
   )
 }
 
