b54ce49ec5b21ffe99ff2e63267cb17d9934157f
HoangXuanTruong <haongtruong4567@gmail.com>
2025-11-21 15:02:08 +0700

    ip

--- codecuathay/ecmascript-fall25-WD20307/react-tour-app/src/App.jsx
+++ codecuathay/ecmascript-fall25-WD20307/react-tour-app/src/App.jsx
@@ -1,16 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { useState } from "react";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  const [ison, setIson] = useState(true);
+  const [tour, setTour] = useState([]);
+  const handleClick = () => {
+    const newIson = !ison;
+    console.log(newIson);
+    setIson(newIson);
+}
+  return (
+    <BrowserRouter>
+      <button className="bg-amber-300 px-4 py-2 rounded" onClick={handleClick}>
+        click {ison ? 'bat' : 'tat'}
+      </button>
+ <button className="bg-amber-300 px-4 py-2 rounded" onClick= {()=> handleClickArg(!isOn)}>
+        click {ison ? 'bat' : 'tat'}
+      </button>
+      <Routes>
+        <Route path="/" element={<Home ison={ison} tour={tour} />} />
+        <Route path="/about" element={<About ison={ison} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App; 
+export default App;
--- codecuathay/ecmascript-fall25-WD20307/react-tour-app/src/pages/Home.jsx
+++ codecuathay/ecmascript-fall25-WD20307/react-tour-app/src/pages/Home.jsx
@@ -2,32 +2,22 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Heading from '../components/Heading'
 import TourCard from '../components/TourCard'
-
+import { useState } from 'react'
 function Home() {
-  // mock data
-  const tours = [
-    {
-      id: 1,
-      title: 'Tour Du Lịch Cuba',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 2,
-      title: 'Tour Du Lịch Hoa Ky',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 3,
-      title: 'Tour Du Lịch Nhat Ban',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
-    },
-  ]
+  const [tours, setTours] = useState([]);
+  const loadData =() =>{
+    setTours([
+      {id:1, title:'Tour Ha Noi', image:'https://picsum.photos/400/300?random=1'},
+      {id:2, title:'Tour Sai Gon', image:'https://picsum.photos/400/300?random=2'},
+      {id:3, title:'Tour Da Nang', image:'https://picsum.photos/400/300?random=3'},
+    ])
+  }
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
+      <button onClick={loadData} className='bg-amber-300'>
+        Load Data
+      </button>
       <main>
         {/* <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
           Tour Nội Địa
